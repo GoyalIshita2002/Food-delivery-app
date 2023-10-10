249b2ace8c88d803fdb93824a8ad421954ae4fe1
AvinashWebkorps <avinash@webkorps.com>
2023-10-10 12:28:19 +0530

    driver-signout

--- app/controllers/v1/driver/session_controller.rb
+++ app/controllers/v1/driver/session_controller.rb
@@ -1,10 +1,19 @@
 class V1::Driver::SessionController < ApplicationController
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!  , only: [:create]
 
   def create
     if driver.present? && ((driver.otp == driver_params[:otp]) || (driver_params[:otp]&.to_i == 1234))
     else
       render json: {status: {code: "400", error: "Invalid parameters"}}, status: :bad_request and return
+    end
+  end
+
+  def destroy
+    if @current_driver.present?
+      @current_driver.update_jti
+      render json: { status: 200, message: I18n.t('session.signout.success')},status: :ok
+    else
+      render json: { status:401, message: I18n.t('session.signout.failure')},status: :unauthorized
     end
   end
 
