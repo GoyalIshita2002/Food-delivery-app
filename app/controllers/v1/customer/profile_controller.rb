0848c21c04208ce96c6185022e59cc6b28f1bb16
AvinashWebkorps <avinash@webkorps.com>
2023-10-23 11:48:01 +0530

    change in profile_controller

--- app/controllers/v1/customer/profile_controller.rb
+++ app/controllers/v1/customer/profile_controller.rb
@@ -4,10 +4,8 @@ class V1::Customer::ProfileController < ApplicationController
    # unless (CustomerOtp.find_by(otp: params[:otp])&.customer_id == current_customer.id)
      #   render json: { status: { code: "400", message: "Invalid OTP" }}, status: :bad_request and return
      # end
-     customer_otp = CustomerOtp.find_by(otp: params[:otp])&.customer
-      if customer_otp
-        customer.update(is_verified: true)
-        @customer=customer
+      if current_customer.customer_otp.otp == params[:otp]
+        current_customer.update(is_verified: true)
       else
         render json: { status: { success: false, code: "400", message: "Invalid OTP" }}, status: :bad_request and return
        end    
@@ -46,9 +44,9 @@ class V1::Customer::ProfileController < ApplicationController
   end
 
   def resend_otp
-    if customer.present?
-      customer.customer_otp.destroy if customer.customer_otp.present?
-      otp = customer.verification_otp
+    if current_customer.present?
+      current_customer.customer_otp.destroy if current_customer.customer_otp.present?
+      otp = current_customer.verification_otp
       render json: { status: { success: true, code: "200", message: "OTP resent successfully", new_otp: otp}}
     else
       render json: { status: { code: "400", message:"customer is not present" }}, status: :bad_request and return
@@ -102,7 +100,7 @@ class V1::Customer::ProfileController < ApplicationController
     params.permit(:phone)
   end
 
-  def customer
-    Customer.find_by(email: params[:email])
+  def current_customer
+    @current_customer ||= Customer.find_by(email: params[:email])
   end
 end 
