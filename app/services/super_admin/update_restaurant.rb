c987188c760e72870f067b5f31cbfb92d058349f
AvinashWebkorps <avinash@webkorps.com>
2023-11-06 15:07:30 +0530

    resolve issues releated to update api of restaurant

--- app/services/super_admin/update_restaurant.rb
+++ app/services/super_admin/update_restaurant.rb
@@ -13,14 +13,20 @@ class SuperAdmin::UpdateRestaurant < ApplicationService
       @restaurant.restaurant_address.update(restaurant_address_params) if restaurant_address_params.present?
       open_hour_params.each do |open_params|
         open_hour = @restaurant.open_hours.find_by(id: open_params[:id])
-        open_hour&.update(open_params.except(:split_hours))
-        open_hour&.split_hours.find_by(id: params[:id])&.update(open_params['split_hour'])
+        if open_hour.present?
+          open_hour.update(open_params.except(:split_hours))
+          split_hour_params = open_params['split_hour']
+          if split_hour_params.present?
+            split_hour = open_hour.split_hours.find_by(id: params[:id])
+            split_hour&.update(split_hour_params)
+          end
+        end
       end
       @restaurant.customer_margin.update(margin_percent: customer_margin) if customer_margin.present?
       @restaurant.restaurant_margin.update(margin_percent: restaurant_margin) if restaurant_margin.present?
     end
   end
-
+  
   def restaurant_params 
     params.require(:restaurant).permit(:name, :registration_date, :phone, :lock_menu, :main_image)
   end
