0894501d38780b22abd4d0fbdf7ceebee6f1e680
AvinashWebkorps <avinash@webkorps.com>
2023-11-15 13:28:03 +0530

    filter work on admin on restaurant

--- app/controllers/v1/customer/restaurants_controller.rb
+++ app/controllers/v1/customer/restaurants_controller.rb
@@ -2,7 +2,15 @@ class V1::Customer::RestaurantsController < ApplicationController
   include Pagy::Backend
 
   def index
-    @pagy, @restaurants = pagy(Restaurant.opened, items: params[:per_page]&.to_i)
+    if params[:search].present?
+      search = params[:search].downcase
+      restaurants_by_rating = Restaurant.where('avg_rating::text LIKE ?', "%#{search}%").opened
+      dish_restaurant_ids = Dish.where('lower(name) LIKE ?', "%#{search}%").pluck(:restaurant_id).uniq
+      restaurants_by_dish = Restaurant.where(id: dish_restaurant_ids)
+      @pagy, @restaurants = pagy(restaurants_by_rating.or(restaurants_by_dish), items: params[:per_page]&.to_i)
+    else
+      @pagy, @restaurants = pagy(Restaurant.opened, items: params[:per_page]&.to_i)
+    end
   end
 
   def show
@@ -11,4 +19,5 @@ class V1::Customer::RestaurantsController < ApplicationController
       render json: { code: { status: "404", error: "invalid restaurant ID"}}
     end
   end
+
 end
